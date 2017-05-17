089f78b29008a0603ad71773a1299b81cb67e4e2
Sceluswe <emilcbm@gmail.com>
2017-05-17 18:21:04 +0200

    displaying laureates data

--- kmom02/nobel/www/js/views/nobelInfo.js
+++ kmom02/nobel/www/js/views/nobelInfo.js
@@ -8,46 +8,16 @@ module.exports = {
     oninit: function (vnode) {
         console.log("Year we're trying to get the info of: " + vnode.attrs.year);
         Nobel.load(vnode.attrs.year);
-
-        //console.log("oninit year: " + Nobel.current.year);
-        //console.log("oninit prizes: " + Nobel.current.prizes[0].category);
     },
 
     view: function () {
-        //console.log("THIS prizes:" + typeof Nobel.current.prizes);
-        // Loop through all categories.
-        //Nobel.current.prizes.forEach(function (item, index, array) {
-            // Get laureates for this category.
-            //var currentLaureates = [];
-            /*item.laureates.forEach(function (laureate) {
-                currentLaureates.push(m("div", [
-                    m("h2", laureate.firstname + laureate.lastname),
-                    //m("p", laureate.motivation)
-                ]));
-            });*/
-
-            // Put laureate vnodes into this category.
-            /*var content = m("div", [
-                m("h1", item.category),
-                m("div", currentLaureates)
-            ])*/
-        //});
-        // console.log(Nobel.current.prizes);
-        //console.log(Nobel.current.category);
-        // console.log(Nobel.current.laureates);
-        // console.log(Nobel.current.categories[0]);
-        //console.log(Nobel.current.names[0]);
-        //console.log(Nobel.current.names[0][0]);
-
         var thename = "";
         var themotivation = "";
-        // The array solution.
-        // Nobel.current.names.forEach(function (item) {
-        //     thename = item[0];
-        // });
+        var thecategory = "";
 
         // The object solution.
         Nobel.current.categories.forEach(function (category) {
+            thecategory = category.name;
             thename = category.winnerNames[0];
             themotivation = category.winnerMotivations[0];
         });
@@ -55,17 +25,23 @@ module.exports = {
         console.log(thename);
         console.log(themotivation);
 
-        return m("div", [
-            m("h1", Nobel.current.year),
-            m("h2", thename),
-            m("p", themotivation),
-            // m("h1", Nobel.current.names[0][0]),
-            // m("h1", Nobel.current.categories[0].names[0]),
-            // m("h1", Nobel.current.names[0]),
-            //m("p", Nobel.current.prizes[0].category),
-            //m("h2", Nobel.categories[0]),
-            //m("p", Nobel.lauretes[0][0].name),
-            //m("p", Nobel.lauretes[0][0].motivation),
-        ]);
+        // The array with all HTML elements.
+        var arr_vnodes = [];
+
+        // The object solution.
+        Nobel.current.categories.forEach(function (category) {
+            // Vnode category name.
+            arr_vnodes.push(m("h2", category.name));
+
+            // Get lenght of the arrays.
+            var len = category.winnerNames.length;
+
+            for (var i = 0; i < len; i++) {
+                arr_vnodes.push(m("h3", category.winnerNames[i]));
+                arr_vnodes.push(m("p", category.winnerMotivations[i]));
+            }
+        });
+
+        return m("div", arr_vnodes);
     }
 };
